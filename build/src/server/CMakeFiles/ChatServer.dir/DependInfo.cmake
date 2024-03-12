5d24ff6ee01e6377a67444c4344a8cd91de7c717
root <root@localhost.localdomain>
2024-03-12 12:20:26 +0800

    添加一键编译脚本

---
+++ build/src/server/CMakeFiles/ChatServer.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/CHAT/muduo_ChatServer/src/server/chatserver.cpp" "/root/CHAT/muduo_ChatServer/build/src/server/CMakeFiles/ChatServer.dir/chatserver.cpp.o"
+  "/root/CHAT/muduo_ChatServer/src/server/chatservice.cpp" "/root/CHAT/muduo_ChatServer/build/src/server/CMakeFiles/ChatServer.dir/chatservice.cpp.o"
+  "/root/CHAT/muduo_ChatServer/src/server/db/db.cpp" "/root/CHAT/muduo_ChatServer/build/src/server/CMakeFiles/ChatServer.dir/db/db.cpp.o"
+  "/root/CHAT/muduo_ChatServer/src/server/main.cpp" "/root/CHAT/muduo_ChatServer/build/src/server/CMakeFiles/ChatServer.dir/main.cpp.o"
+  "/root/CHAT/muduo_ChatServer/src/server/model/friendmodel.cpp" "/root/CHAT/muduo_ChatServer/build/src/server/CMakeFiles/ChatServer.dir/model/friendmodel.cpp.o"
+  "/root/CHAT/muduo_ChatServer/src/server/model/groupmodel.cpp" "/root/CHAT/muduo_ChatServer/build/src/server/CMakeFiles/ChatServer.dir/model/groupmodel.cpp.o"
+  "/root/CHAT/muduo_ChatServer/src/server/model/offlinemsg.cpp" "/root/CHAT/muduo_ChatServer/build/src/server/CMakeFiles/ChatServer.dir/model/offlinemsg.cpp.o"
+  "/root/CHAT/muduo_ChatServer/src/server/model/usermodel.cpp" "/root/CHAT/muduo_ChatServer/build/src/server/CMakeFiles/ChatServer.dir/model/usermodel.cpp.o"
+  "/root/CHAT/muduo_ChatServer/src/server/redis/redis.cpp" "/root/CHAT/muduo_ChatServer/build/src/server/CMakeFiles/ChatServer.dir/redis/redis.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../include/server"
+  "../include/server/db"
+  "../include/server/model"
+  "../include/server/redis"
+  "../thirdparty"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
